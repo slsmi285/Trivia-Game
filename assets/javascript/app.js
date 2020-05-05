9bb038127878d7781043ae62714ba0bd9617b764
Sandra Smith <toosandra@gmail.com>
2020-05-05 00:01:05 -0400

    Pseudo code in javascript and commented in html.

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+//Advanced Trivia Game
+
+//The Game has 8 questions, and each question has 4 multiple choice answers.
+//Questions and answers -- ARRAYS and ARRAYS.
+
+//User starts the game with an onclick.
+
+//Computer Randomly chooses a question.  Quesiton is displayed, 4 multiple choice answers are displayed.
+//30 second timer starts when question is populated.
+
+//If user chooses correct answer -- correct answer is true.
+//Player wins.
+//Prompt displays "You Win: Correct Answer Displayed - above image or video image"
+
+//for next random question, there is a 3 second delay before next question is displayed.
+
+//if the player runs out of time, a prompt will be displayed "You ran out of time!  - display correct answer above picture".
+// If the player chooses the wrong answer, a prompt will be displayed "Wrong Answer! - display correct answer above picture"
+
+//There is a timer, 3 types of scores and 2 types of resets.
+    //timer is 30 seconds
+    // 3 types of scores -- final page: Correct Answers, Incorrect Answers, Unanswered.
+    //Final page prompts  - displays: All done! Here's how you did!
+    //Final page - reset - start over onlclick button
